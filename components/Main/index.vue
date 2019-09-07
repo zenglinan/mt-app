23b0b5348ccee23ecc4583c6a8bc4c9ef48f2778
datou <1392767180@qq.com>
2019-09-08 01:17:12 +0800

    添加轮播图

--- components/Main/index.vue
+++ components/Main/index.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="m-main">
     <m-menu></m-menu>
+    <m-banner></m-banner>
   </div>
 </template>
 
 <script>
   import menu from './menu'
+  import banner from './banner'
   export default {
     name: 'm-main',
     components:{
-      'm-menu': menu
+      'm-menu': menu,
+      'm-banner': banner
     }
   }
 </script>
 
 <style scoped lang="scss">
+  .m-main{
+    display: flex;
+  }
 </style>
