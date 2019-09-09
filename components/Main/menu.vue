299bce7643e358862b7ed63e7a2b4fc592eccdf4
datou <1392767180@qq.com>
2019-09-09 23:13:53 +0800

    解决nav最后一个导航下拉菜单被轮播遮挡的bug

--- components/Main/menu.vue
+++ components/Main/menu.vue
@@ -1,6 +1,6 @@
 <template>
-  <el-container class="m-menu">
-    <el-aside style="width: 228px;">
+  <div class="m-menu">
+    <el-aside style="width: 227px;">
       <el-menu>
         <h3>全部分类</h3>
         <el-menu-item v-for="(item, idx) in menu" :key="idx">
@@ -8,17 +8,17 @@
         </el-menu-item>
       </el-menu>
     </el-aside>
-    <el-container class="card" v-show="false">
-      <el-main>
-        <template v-for="menuItem in menu" v-show="">
-          <dl v-for="(childMenu, idx) in menuItem.child" :key="idx">
-            <h3>{{childMenu.title}}</h3>
-            <dd v-for="(content, id) in childMenu.child" :key="id">{{content}}</dd>
-          </dl>
-        </template>
-      </el-main>
-    </el-container>
-  </el-container>
+<!--    <el-container class="card" v-show="false">-->
+<!--      <el-main>-->
+<!--        <template v-for="menuItem in menu" v-show="">-->
+<!--          <dl v-for="(childMenu, idx) in menuItem.child" :key="idx">-->
+<!--            <h3>{{childMenu.title}}</h3>-->
+<!--            <dd v-for="(content, id) in childMenu.child" :key="id">{{content}}</dd>-->
+<!--          </dl>-->
+<!--        </template>-->
+<!--      </el-main>-->
+<!--    </el-container>-->
+  </div>
 </template>
 
 <script>
@@ -44,27 +44,28 @@
   @import "@/assets/scss/fontClass.scss";
 
   .m-menu {
+    font-family: 'meituan';
     flex-grow: 0;
     transform: translateY(-45px);
-    height: 474px;
+    border: 1px solid #E5E5E5;
+    box-sizing: border-box;
     .el-aside {
-      width: 230px;
-      border: 1px solid #E5E5E5;
-      margin-left: 36px;
-
+      height: 472px;
+      .el-menu{
+        border: none;
+      }
       h3 {
-        padding: 15px 0 20px 15px;
+        padding: 16px 0 20px 16px;
         height: 57px;
       }
 
       .el-menu-item {
         cursor: default;
-        padding: 3px 12px;
         height: auto;
         line-height: normal;
         color: #646464;
         transition: none;
-
+        padding: 3px 12px 4px;
         &:hover {
           background: rgba(255, 150, 0, 0.08);
           font-weight: bold;
